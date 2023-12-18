0a25a41e7af214b7228655083a25badd5c305b99
Votre Nom <tchjohanna@icloud.com>
2023-12-18 19:25:33 +0100

    maj styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./css/styles.scss";
+import "./assets/css/styles.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
