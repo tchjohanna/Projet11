51712873cd62471d39bd6280cab1cce93bfad277
Votre Nom <tchjohanna@icloud.com>
2023-12-20 12:24:24 +0100

    maj composant navlink avec react rooter dom

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -9,79 +9,63 @@ import {
   clearState,
 } from '../../features/UserSlices';
 
-
-
 function Header() {
-  // Récupération du token depuis le stockage local
   let token = localStorage.getItem('token');
   const navigate = useNavigate();
-
-  // Fonction de déconnexion
-  const logout = () => {
-    localStorage.removeItem('token');
-    navigate('/signIn');
-  };
-
   const dispatch = useDispatch();
+  const { isError, username, firstName } = useSelector(userSelector);
 
-  // Utilisation de useEffect pour charger les données de l'utilisateur lors du chargement du composant
   useEffect(() => {
-    if (token != null) {
-      dispatch(fetchUserBytoken({ token: token }));
+    if (token) {
+      dispatch(fetchUserBytoken({ token }));
     }
   }, [dispatch, token]);
 
-  const { isError, username, firstName } = useSelector(userSelector);
-
-  // Nettoyage des données du composant lorsqu'il est démonté
   useEffect(() => {
     return () => {
       dispatch(clearState());
     };
   }, [dispatch]);
 
-  // Gestion des erreurs
   useEffect(() => {
     if (isError) {
       dispatch(clearState());
     }
   }, [isError, dispatch]);
 
-  let menuLink;
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/signIn');
+  };
 
-  // Création des liens du menu en fonction de la présence ou de l'absence du token
-  if (token != null) {
-    menuLink = (
-      <>
-        <NavLink activeClassName='active' className='main-nav-item' to='/user'>
-  <i className='fa fa-user-circle'></i>
-  {username != null ? username : firstName}
-</NavLink>
-
-        <Link className='main-nav-item' onClick={logout}>
-          <i className='fa fa-sign-out'></i>
-          Sign Out
-        </Link>
-      </>
-    );
-  } else {
-    menuLink = (
-      <NavLink activeClassName='active' className='main-nav-item' to={'/user'}>
-  <i className='fa fa-user-circle-o'></i>
-  {username != null ? username : firstName}
-</NavLink>
-
-    );
-  }
+  let menuLink = token ? (
+    <>
+      <NavLink
+        className={({ isActive }) => isActive ? 'main-nav-item active' : 'main-nav-item'}
+        to='/user'
+      >
+        <i className='fa fa-user-circle'></i>
+        {username || firstName}
+      </NavLink>
+      <Link className='main-nav-item' onClick={logout}>
+        <i className='fa fa-sign-out'></i>
+        Sign Out
+      </Link>
+    </>
+  ) : (
+    <NavLink
+      className={({ isActive }) => isActive ? 'main-nav-item active' : 'main-nav-item'}
+      to='/user'
+    >
+      <i className='fa fa-user-circle-o'></i>
+      {username || firstName}
+    </NavLink>
+  );
 
   return (
     <nav className='main-nav'>
-      <Link className='main-nav-logo' to={'/'}>
-        <img
-          className='main-nav-logo-image'
-          src={Logo}
-          alt='Argent Bank Logo'
-        />
+      <Link className='main-nav-logo' to='/'>
+        <img className='main-nav-logo-image' src={Logo} alt='Argent Bank Logo' />
         <h1 className='sr-only'>Argent Bank</h1>
       </Link>
       <div>{menuLink}</div>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './utils/global-style.scss'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import User from './pages/User';
 import Error from './pages/Error';
-import { Provider } from 'react-redux'
-import store from './utils/store'
-
+import './utils/global-style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    
+    <React.StrictMode>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/user" element={<User />} />
-          {/* Conditional render for the error page */}
-          <Route path="*" render={(props) => {
-            throw new Error('An unexpected error occurred.');
-          }} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-    
+    </React.StrictMode>
   </Provider>
 );
